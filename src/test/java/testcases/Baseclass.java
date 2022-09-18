dbd07407a5f3e09e462a71af84c5dc3fcb887969
lparthiban2712 <dasthaan.gmail.com>
2022-09-18 12:10:24 +0530

    recent changes

--- src/test/java/testcases/Baseclass.java
+++ src/test/java/testcases/Baseclass.java
@@ -3,6 +3,7 @@ package testcases;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.lang.reflect.Method;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.poi.xssf.usermodel.XSSFSheet;
@@ -16,10 +17,15 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.BeforeTest;
 
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+
 public class Baseclass {
 	WebDriver driver;
 	XSSFWorkbook workbook;
 	XSSFSheet sheet;
+	ExtentReports report;
+	ExtentTest test;
 	
 	@BeforeTest
 	public void datasetup() throws IOException
@@ -27,6 +33,8 @@ public class Baseclass {
 		FileInputStream fis = new FileInputStream("exceldata.xlsx");
 		workbook=new XSSFWorkbook(fis);
 		sheet=workbook.getSheet("Sheet1");
+		report=new ExtentReports("ExtentReports.html");
+		
 		
 	}
 	
@@ -34,22 +42,26 @@ public class Baseclass {
 	public void datacleanup() throws IOException
 	{
 		workbook.close();
+		report.flush();
+		report.close();
 	}
 	
 	@BeforeMethod
-	public void setup()
+	public void setup(Method method)
 	{
+		test = report.startTest(method.getName());
 		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
 		driver=new ChromeDriver();
 		driver.get("https://www.simplilearn.com/");
 		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(5000, TimeUnit.MILLISECONDS);
+		driver.manage().timeouts().implicitlyWait(20000, TimeUnit.MILLISECONDS);
 	}
 
 	@AfterMethod
 	
 	public void teardown()
 	{
+		report.endTest(test);
 		driver.close();
 	}
 }
--- src/test/java/testcases/SimplilearnLoginTest.java
+++ src/test/java/testcases/SimplilearnLoginTest.java
@@ -11,6 +11,8 @@ import org.testng.Assert;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
+import com.relevantcodes.extentreports.LogStatus;
+
 import pages.Loginpage;
 
 public class SimplilearnLoginTest extends Baseclass{
@@ -18,7 +20,7 @@ public class SimplilearnLoginTest extends Baseclass{
 	@Test
 	public void Test1()
 	{
-		
+		test.log(LogStatus.INFO, "Test1 started");
 		Loginpage lp=new Loginpage(driver);
 		lp.Login("correctusername", "correctpassword");
 		WebElement error=driver.findElement(By.id("error_box"));
@@ -36,7 +38,7 @@ public class SimplilearnLoginTest extends Baseclass{
 	@Parameters({"email","pwd"})
 	public void Test2(String uname, String passwd)
 	{
-		
+		test.log(LogStatus.INFO, "Test2 started");
 		Loginpage lp=new Loginpage(driver);
 		lp.Login(uname, passwd);
 		
@@ -47,6 +49,8 @@ public class SimplilearnLoginTest extends Baseclass{
 	@Test
 	public void Test3()
 	{
+		
+		test.log(LogStatus.INFO, "Test3 started");
 		String uname=sheet.getRow(1).getCell(0).getStringCellValue();
 		String passwd=sheet.getRow(1).getCell(1).getStringCellValue();
 		Loginpage lp=new Loginpage(driver);
